b18f05abf941450f53cc2e2f98549f06ce256b70
kai-city <1830170041@qq.com>
2021-05-24 22:07:07 +0800

    1.若搜索框为打开状态，点击列表时会收起 2.增加启动图 3.申请了读写文件权限

---
+++ app/src/main/java/top/niunaijun/blackdex/view/base/WelcomeActivity.kt
@@ -0,0 +1,19 @@
+package top.niunaijun.blackdex.view.base
+
+import android.content.Intent
+import android.os.Bundle
+import top.niunaijun.blackdex.view.main.MainActivity
+
+/**
+ *
+ * @Description:
+ * @Author: wukaicheng
+ * @CreateDate: 2021/5/24 21:49
+ */
+class WelcomeActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
